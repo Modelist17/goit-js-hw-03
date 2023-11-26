c58426cbc0f4334a68f98d61ed49f59e9726b75c
Modelist17 <hunteruboat@gmail.com>
2023-11-26 23:43:09 +0100

    biba i boba

--- js/task-1.js
+++ js/task-1.js
@@ -1,3 +1,4 @@
+// task 1:
 // function makeTransaction(quantity, pricePerDroid) {
 //   const totalPrice = quantity * pricePerDroid;
 //   return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
@@ -7,22 +8,36 @@
 // console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
 // console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
 
-
-function makeTransaction(quantity, pricePerDroid, customerCredits) {
+// task 2:
+// function makeTransaction(quantity, pricePerDroid, customerCredits) {
   
-  const totalPrice = quantity * pricePerDroid;
+//   const totalPrice = quantity * pricePerDroid;
 
-  // Перевірка чи клієнт може оплатити замовлення
-  if (totalPrice > customerCredits) {
-    return "Insufficient funds!";
-  } else {
-    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-  }
+//   // Перевірка чи клієнт може оплатити замовлення
+//   if (totalPrice > customerCredits) {
+//     return "Insufficient funds!";
+//   } else {
+//     return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+//   }
+// }
+
+// // Перевірка роботи
+// console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
+// console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
+// console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
+// console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
+// console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
+
+// task 3:
+function slugify(title) {
+  const lowercaseTitle = title.toLowerCase();
+  const words = lowercaseTitle.split(' ');
+  const slug = words.join('-');
+  return slug;
 }
 
-// Перевірка роботи
-console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
-console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
-console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
+// Перевірка роботи функції
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
--- js/task-2.js
+++ js/task-2.js
@@ -1,3 +1,4 @@
+// task 1:
 // function getShippingMessage(country, price, deliveryFee) {
 //   const totalPrice = price + deliveryFee;
 //   return `Shipping to ${country} will cost ${totalPrice} credits`;
@@ -7,19 +8,38 @@
 // console.log(getShippingMessage('Germany', 80, 20)); // "Shipping to Germany will cost 100 credits"
 // console.log(getShippingMessage('Sweden', 100, 20)); // "Shipping to Sweden will cost 120 credits"
 
-function formatMessage(message, maxLength) {
-  // Перевірка довжини рядка
-  if (message.length <= maxLength) {
-    return message;
+// task 2:
+// function formatMessage(message, maxLength) {
+//   // Перевірка довжини рядка
+//   if (message.length <= maxLength) {
+//     return message;
+//   } else {
+//     return message.slice(0, maxLength) + "...";
+//   }
+// }
+
+// // Перевірка роботи функції
+// console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
+// console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
+// console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
+// console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
+// console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
+// console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
+
+// TASK 2:
+function makeArray(firstArray, secondArray, maxLength) {
+  const newArray = firstArray.concat(secondArray);
+  if (newArray.length > maxLength) {
+    return newArray.slice(0, maxLength);
   } else {
-    return message.slice(0, maxLength) + "...";
+    return newArray;
   }
 }
 
 // Перевірка роботи функції
-console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
-console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
-console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
-console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
-console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
-console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
