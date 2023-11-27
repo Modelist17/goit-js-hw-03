9c3b1289cd9226869da90837da78fc45b3c2b7d9
Modelist17 <hunteruboat@gmail.com>
2023-11-27 11:22:27 +0100

    M1A2 Abrams SEPv2

--- js/task-2.js
+++ js/task-2.js
@@ -1,42 +1,16 @@
-// task 1:
-// function getShippingMessage(country, price, deliveryFee) {
-//   const totalPrice = price + deliveryFee;
-//   return `Shipping to ${country} will cost ${totalPrice} credits`;
-// }
 
-// console.log(getShippingMessage('Australia', 120, 50)); // "Shipping to Australia will cost 170 credits"
-// console.log(getShippingMessage('Germany', 80, 20)); // "Shipping to Germany will cost 100 credits"
-// console.log(getShippingMessage('Sweden', 100, 20)); // "Shipping to Sweden will cost 120 credits"
-
-// task 2:
-// function formatMessage(message, maxLength) {
-//   // Перевірка довжини рядка
-//   if (message.length <= maxLength) {
-//     return message;
-//   } else {
-//     return message.slice(0, maxLength) + "...";
-//   }
-// }
-
-// // Перевірка роботи функції
-// console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
-// console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
-// console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
-// console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
-// console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
-// console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
-
-// TASK 2:
+// TASK 3:
 function makeArray(firstArray, secondArray, maxLength) {
-  const newArray = firstArray.concat(secondArray);
-  if (newArray.length > maxLength) {
-    return newArray.slice(0, maxLength);
-  } else {
-    return newArray;
-  }
+  // Об'єдную масиви//
+  const combinedArray = firstArray.concat(secondArray);
+
+  // Обмежую довжину відповідно до maxLength//
+  const resultArray = maxLength > 0 ? combinedArray.slice(0, maxLength) : [];
+
+  return resultArray;
 }
 
-// Перевірка роботи функції
+// Перевірка зазначених тестових випадків
 console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
 console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
 console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
--- js/task-3.js
+++ js/task-3.js
@@ -1,54 +1,26 @@
-// function getElementWidth(content, padding, border) {
-//   const contentWidth = parseFloat(content);
-//   const paddingWidth = parseFloat(padding);
-//   const borderWidth = parseFloat(border);
-
-//   const totalWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth;
-
-//   return totalWidth;
-// }
-
-// console.log(getElementWidth('50px', '8px', '4px')); // 74
-// console.log(getElementWidth('60px', '12px', '8.5px')); // 101
-// console.log(getElementWidth('200px', '0px', '0px')); // 200
-
-// TASK 2:
-// function checkForSpam(message) {
-//   const lowerCaseMessage = message.toLowerCase();
-//   if (lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("sale")) {
-//     return true; // Заборонене слово знайдено - повертає true
-//   } else {
-//     return false; // Заборонені слова відсутні - повертає false
-//   }
-// }
-
-// // Перевірка роботи функції
-// console.log(checkForSpam("Latest technology news")); // false
-// console.log(checkForSpam("JavaScript weekly newsletter")); // false
-// console.log(checkForSpam("Get best sale offers now!")); // true
-// console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-// console.log(checkForSpam("Trust me, this is not a spam message")); // true
-// console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-// console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
 
 // TASK 3:
 function filterArray(numbers, value) {
-  const filteredArray = [];
-
+  let filteredArray = [];
 
   for (let i = 0; i < numbers.length; i++) {
-    
-    if (numbers[i] > value) {
-      filteredArray.push(numbers[i]);
+    const currentNumber = numbers[i];
+
+    if (currentNumber > value) {
+      filteredArray.push(currentNumber);
     }
   }
 
   return filteredArray;
 }
 
-
 console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
 console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
 console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
 console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
 console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+
+// Виклик функції filterArray() з випадковим масивом і числом
+const randomArray = [8, 15, 3, 10, 22, 17];
+const randomValue = 12;
+console.log(filterArray(randomArray, randomValue));
